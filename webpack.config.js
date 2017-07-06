8526dd45f3296052b1e8e384fb36425ee8981ea7
Lee Haugen <haugenlee@leegenes.local>
2017-07-06 13:03:37 -0400

    update paths for new dir structure

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     './index.js',
   ],
   output: {
-    path: path.join(__dirname, 'www'),
+    path: path.join(__dirname, 'public'),
     filename: 'bundle.js',
   },
   module: {
